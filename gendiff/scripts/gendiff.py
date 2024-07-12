33860e58925322ffb2126e14b8614238523d3d12
Alina Li <alinali87@gmail.com>
2024-07-12 10:27:08 +0400

    step 6: add default for format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
     parser.add_argument("-f", "--format", type=str,
-                        help="set format of output")
+                        help="set format of output", default="stylish")
 
     args = parser.parse_args()
     first_file = args.first_file
